fd0be72af4e598a82ab625afa4bb95df57d7cdb5
Mariaesteves <mariaferraesteves@hotmail.com>
2025-04-21 15:01:32 +0100

    solved lab-intro-node

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length= 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort ((a,b)=> a-b);
 
-  get(pos) {}
+    this.length= this.items.length
+  }
 
-  max() {}
+  get(pos) {
+    if (pos<0 || pos> this.items.length){
+      throw new Error ("OutOfBounds")
+    }
+    return this.items[pos]
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error ("EmptySortedList")
+    }
+    return this.items.reduce((a, b) => Math.max(a, b), -Infinity)
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error ("EmptySortedList")
+    }
+    return this.items.reduce((a, b) => Math.min(a, b), +Infinity)
+  }
+  
 
-  avg() {}
+  sum() {
+    if (this.items.length === 0){
+      return 0
+    }
+    return this.items.reduce((a,b)=> a + b, 0)
+  }
+
+  avg() {
+    if (this.items.length === 0){
+      throw new Error ("EmptySortedList")
+    }
+    const sum= this.items.reduce((a,b)=> a + b, 0)
+    return sum/ this.items.length
+  }
 }
 
 module.exports = SortedList;
